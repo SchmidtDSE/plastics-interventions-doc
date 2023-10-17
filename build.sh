84531d9d741d54ffa82badd4a5903a13766fa507
A Samuel Pottinger <sam@gleap.org>
2023-10-17 17:57:47 +0000

    Remove biblatex flag

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir pdfs/
 for file in *.md; do
     output_file="pdfs/$(basename $file .md).pdf"
-    pandoc --biblatex --filter pandoc-citeproc --output=$output_file --bibliography=sources.bib $file
+    pandoc --filter pandoc-citeproc --output=$output_file --bibliography=sources.bib $file
 done
